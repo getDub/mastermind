ad1c925c4c3c8546ac7f0b37b9615deaf85e3b92
getDub <143089345+getDub@users.noreply.github.com>
2024-04-12 17:58:07 +1000

    Laying out classes and seeing how this game is going to work

--- mastermind.rb
+++ mastermind.rb
@@ -1,24 +1,58 @@
-# Codemaker
-colored_pegs = ['Red', 'Green', 'Blue', 'Cyan', 'Magenta', 'Yellow']
-$peg_positions = [[1, 2, 3, 4]]
+#Colour selection
+#Feedback on selection - dots - winner
+#Human code selector
+#Computer guesser
 
-def peg_board
-  row = '-------+-------+-------+-------'
-  $peg_positions.each do |positions|
-    puts positions.join('   |   ').insert(0, '   ')
-    puts row
+#Codemaker
+module Mastermind
+  COLOURS = ['Red', 'Green', 'Blue', 'Cyan', 'Magenta', 'Yellow']
+  
+  
+  
+#Rules intro
+  
+  class GAME
+    
+    @@peg_positions = [[1, 2, 3, 4]]
+    
+    def initialize(human_player_class)
+      @intro = intro
+    end
+    
+    def intro
+      
+    end
+
+    def print_board
+      front_spacer, col_separator, row_separator = '   ', '   |   ', '-------+-------+-------+-------'
+      @@peg_positions.each do |positions|
+        puts positions.join(col_separator).insert(0, front_spacer)
+        puts row
+      end
+    end
+    
+    
   end
-end
-peg_board
 
-def random_code_maker(arr)
-  code = []
-  4.times do 
-    code << arr[Random.rand(0..5)]
+  class Player
+    
   end
-  p code
+
+  class HumanPlayer
+    
+  end
+  
+  class ComputerPlayer
+    
+    def random_code_maker(arr)
+      code = []
+      4.times do 
+        code << arr[Random.rand(0..5)]
+      end
+      p code
+    end
+  
+    random_code_maker(colored_pegs)
+  end
+  
 end
-
-random_code_maker(colored_pegs)
-
-# Codebreaker
