1862d5697be0efb35a60f1e99564b8a272322a44
getDub <143089345+getDub@users.noreply.github.com>
2024-04-19 19:23:21 +1000

    Game class initiates Player class properly now.
    Intro works better and captures player guess.  Player has initializer now and is a mix-in with HumanPlayer class.  Next task is to sort out the random code maker method.

--- mastermind.rb
+++ mastermind.rb
@@ -11,33 +11,48 @@ module Mastermind
   
 #Rules intro
   
-  class GAME
+  class Game
     
     @@peg_positions = [[1, 2, 3, 4]]
-    @@colours = [['Red', 'Grn', 'Blu']]
+    #@@colours = [['Red', 'Green', 'Blue', 'Pink', 'Yellow']]
 
-    attr_accessor :name
+    attr_accessor :codebreaker
+    attr_writer :num_of_attempts
 
     def initialize(human_player_class)
+      @get_name = get_name
+      @codebreaker = human_player_class.new
       @intro = intro
-      @name = name
+      #@name = name
     end
+    
+    @guesses = []
 
-    def name
-      @name
+    def play_game
+      12
     end
     
     def get_name
       puts 'Hi player, please enter your name.'
-      player_name = gets
     end
 
     def intro
-      puts "Thanks #{name}, Welcome to Mastermind.
-      Can you guess the 4 colours chosen by the codemaker? 
+      puts "Thanks #{self.codebreaker.name}, Welcome to Mastermind.
+      Can you guess the 4 colours chosen by the codemaker?
       There are 6 possible colours to choose from, (Red, Green, Blue, Pink, Yellow).
       You have 12 attempts to break the code.
-      Please enter your first 4 guesses."
+      Please enter your first 4 guesses. no commas just a space between each colour."
+      self.guess
+    end
+
+    def guess
+      player_input = gets.chomp
+      @guesses = player_input.split
+      # p @guesses
+    end
+
+    def compare_code
+      @guesses ==       
     end
 
     def feedback
@@ -61,26 +76,39 @@ module Mastermind
   end
 
   class Player
-    
+    attr_accessor :name
+
+    def initialize
+      @name = gets.chomp
+    end
   end
 
-  class HumanPlayer
+  class HumanPlayer < Player
     
   end
   
   class ComputerPlayer
+    attr_accessor :random_code_maker
+    attr_reader :code
+    def initialize
+      @random_code_maker = random_code_maker(COLOURS)
+    end
     
+    @code = []
+
     def random_code_maker(arr)
       code = []
       4.times do 
-        code << arr[Random.rand(0..5)]
+        @code << arr[Random.rand(0..5)]
       end
-      p code
+      p '? | ? | ? | ?'
+      p @code
     end
   
-    random_code_maker(colored_pegs)
   end
   
 end
 
 include Mastermind
+
+Game.new(HumanPlayer)
