e490e07265976d6cd7787ffc981c4dbffdd30d56
getDub <143089345+getDub@users.noreply.github.com>
2024-04-25 16:21:59 +1000

    Random code maker method works
    I had issues with nil and trying to fill an array of a class instance but kept getting nil class no method error. Im still not sure why nil does what it does.

--- mastermind.rb
+++ mastermind.rb
@@ -5,7 +5,7 @@
 
 #Codemaker
 module Mastermind
-  COLOURS = ['Red', 'Green', 'Blue', 'Cyan', 'Pink', 'Yellow']
+  COLOURS = ['Red', 'Green', 'Blue', 'Cyan', 'Pink', 'Yellow'].freeze
   
   
   
@@ -14,46 +14,77 @@ module Mastermind
   class Game
     
     @@peg_positions = [[1, 2, 3, 4]]
-    #@@colours = [['Red', 'Green', 'Blue', 'Pink', 'Yellow']]
 
+    
     attr_accessor :codebreaker
-    attr_writer :num_of_attempts
+    
+    @guesses = []
+    @code = Array.new
 
     def initialize(human_player_class)
       @get_name = get_name
       @codebreaker = human_player_class.new
       @intro = intro
-      #@name = name
+      # @codemaker = ComputerPlayer.new
     end
     
-    @guesses = []
 
     def play_game
-      12
+      random_code_maker
+      get_guess
+      # correct_color_and_position?
+      # has_won?
     end
     
     def get_name
       puts 'Hi player, please enter your name.'
     end
-
+    
     def intro
-      puts "Thanks #{self.codebreaker.name}, Welcome to Mastermind.
+      puts "Thanks #{@codebreaker.name}, Welcome to Mastermind.
       Can you guess the 4 colours chosen by the codemaker?
       There are 6 possible colours to choose from, (Red, Green, Blue, Pink, Yellow).
       You have 12 attempts to break the code.
       Please enter your first 4 guesses. no commas just a space between each colour."
-      self.guess
+    end
+    
+
+    def random_code_maker
+      nums = []
+      4.times do
+        nums << rand(1..4)
+        
+      end
+      # puts '    ?    |    ?    |    ?    |    ?'
+      # p nums
     end
 
-    def guess
+    # def code_has_been_made(rando_code)
+    #   puts '    ?    |    ?    |    ?    |    ?'
+    # end
+
+    def get_guess
       player_input = gets.chomp
       @guesses = player_input.split
-      # p @guesses
+      p @guesses
+    end
+    
+
+    def has_won?
+      if @guesses == COLOURS
+        p 'You won!'
+      else
+        p 'No luck, try again'
+      end
     end
 
-    def compare_code
-      @guesses ==       
-    end
+    # def correct_color_and_position?
+      # correct_combo = []
+      # (1..4).each do |posi|
+      #   correct_combo << if @guesses[posi] == @code[posi]
+      #   p correct_combo
+      # end
+    # end
 
     def feedback
 
@@ -71,8 +102,6 @@ module Mastermind
         puts positions.join(col_separator).insert(0, front_spacer)
       end
     end
-    
-    
   end
 
   class Player
@@ -89,21 +118,22 @@ module Mastermind
   
   class ComputerPlayer
     attr_accessor :random_code_maker
-    attr_reader :code
+    # attr_reader :code
+    @@code = []
+
     def initialize
       @random_code_maker = random_code_maker(COLOURS)
     end
     
-    @code = []
 
-    def random_code_maker(arr)
-      code = []
-      4.times do 
-        @code << arr[Random.rand(0..5)]
-      end
-      p '? | ? | ? | ?'
-      p @code
-    end
+    # def random_code_maker(arr)
+    #   # code = []
+    #   4.times do 
+    #     @@code << arr[Random.rand(0..5)]
+    #   end
+    #   puts '    ?    |    ?    |    ?    |    ?'
+    #   p @@code
+    # end
   
   end
   
@@ -111,4 +141,4 @@ end
 
 include Mastermind
 
-Game.new(HumanPlayer)
+Game.new(HumanPlayer).play_game
