a4ba2f37cd42b881516dcc43128bdaa71c7689fb
getDub <143089345+getDub@users.noreply.github.com>
2025-06-17 19:57:23 +1000

    Removed require_relative colourables

--- lib/colourables.rb
+++ lib/colourables.rb
@@ -1,3 +1,3 @@
-module Colourables
-COLOURS = ['red', 'green', 'blue', 'cyan', 'pink', 'yellow'].freeze
-end
+# module Colourables
+# COLOURS = ['red', 'green', 'blue', 'cyan', 'pink', 'yellow'].freeze
+# end
