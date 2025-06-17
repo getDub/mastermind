51123f9528731441ebb67730e492abec65f11cb9
getDub <143089345+getDub@users.noreply.github.com>
2025-06-17 19:52:25 +1000

    Removed mixed in module colourables and added code_to_break method that's called from game_setup

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -2,12 +2,16 @@
 
 class ComputerPlayer #< Player
   attr_reader :code, :colours
-  include Colourables
+  # include Colourables
   def initialize
-    @colours = COLOURS
-    @code = COLOURS.sample(4)
+    # @colours = COLOURS
+    # @code = COLOURS.sample(4)
+    # @code = game.COLOURS.sample(4)
   end
  
+  def code_to_break(colours)
+    @code = colours.sample(4)
+  end
 end
 
   
